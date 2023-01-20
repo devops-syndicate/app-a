0ea4092c7993ef4919f75ca09eb45c6beb2736ed
sselzer <sascha.selzer@innoq.com>
2023-01-20 16:13:33 +0100

    Replace prometheus middleware

--- server.js
+++ server.js
@@ -3,29 +3,24 @@ const http = require("http");
 const express = require("express");
 const logger = require("./logger");
 const app = express();
-const promBundle = require("express-prom-bundle");
+const metrics = require("express-prometheus-metrics");
 const { createTerminus } = require("@godaddy/terminus");
-const Pyroscope = require('@pyroscope/nodejs');
+const Pyroscope = require("@pyroscope/nodejs");
 const APP_NAME = "app-a";
 const PORT = 8080;
 
 Pyroscope.init({
   serverAddress: process.env.PYROSCOPE_URL || "http://pyroscope:4040",
-  appName: APP_NAME
+  appName: APP_NAME,
 });
 
 Pyroscope.start();
 
-const metricsMiddleware = promBundle({
-  includeMethod: true,
-  includePath: true,
-  includeStatusCode: true,
-  includeUp: true,
-  customLabels: { app: APP_NAME },
-  promClient: { collectDefaultMetrics: {} },
-});
-
-app.use(metricsMiddleware);
+app.use(
+  metrics({
+    metricsPath: "/metrics",
+  })
+);
 
 app.get("/", (_, res) => {
   logger.info("Call hello endpoint");
